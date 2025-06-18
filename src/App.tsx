a864ed30ed2344949c478e79156bc87e678d87ea
kamilarr <kamilarrr21@gmail.com>
2025-06-18 18:07:10 +0700

    Article done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
+import Article from "./pages/Article";
+import NotFound from "./pages/NotFound"; 
 
 const queryClient = new QueryClient();
 
@@ -16,7 +17,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/article" element={<Article />} /> {/* ✅ Perbaiki path dan komponen */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/Article.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Header } from '@/components/Header';
+import { Hero } from '@/components/Hero';
+import { ArticleContent } from '@/components/ArticleContent';
+import { Footer } from '@/components/Footer';
+
+const Index: React.FC = () => {
+  return (
+    <div className="bg-white flex flex-col min-h-screen">
+      <Header />
+      
+      <Hero
+        title="What's the Best Skin-Care Routine?"
+        backgroundImage="https://cdn.builder.io/api/v1/image/assets/58cd6d364fff4966b7ea43a323aeb3d1/64ccff1e4e3051dbed213384a58a5fc96cb16b06?placeholderIfAbsent=true"
+      />
+        <ArticleContent />
+      
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
